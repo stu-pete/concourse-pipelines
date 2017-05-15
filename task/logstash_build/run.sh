3025edadddd6fdafde9a1db74e9b21b58082bf06
Konstantin Kiess <konstantin.kiess@volkswagen.de>
2017-05-15 14:29:56 +0200

    output is compressed to tarball

--- task/logstash_build/run.sh
+++ task/logstash_build/run.sh
@@ -7,6 +7,8 @@ apt-get install openjdk-8-jdk -y
 
 cd logstash_git
 rake bootstrap
+./bin/logstash-plugin install logstash-filter-translate
+mkdir build_output
 
+tar -vczf build_output/logstash.tar.gz bin
 
-xxx
