c9e19a3daba0d2e28947c6966f359e3b24e1c07d
Konstantin Kiess <konstantin.kiess@volkswagen.de>
2017-05-16 09:37:16 +0200

    added artifact output to task

--- task/logstash_build/run.sh
+++ task/logstash_build/run.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
-echo "yay no_proxy"
 
-echo "Installing jdk because docker image misses tools.jar"
+echo "ReInstalling jdk because docker image misses tools.jar"
+
 apt-get update
 apt-get install openjdk-8-jdk -y
 
 cd logstash_git
-rake bootstrap
+
+echo "------	  RAKING LOGSTASH	-------"
+
+rake bootstrap --trace
+
+echo "------	    RAKING DONE  	-------"
+
+echo "------ INSTALLING TRANSLATE PLUGIN ------"
+
 ./bin/logstash-plugin install logstash-filter-translate
 mkdir build_output
 
-tar -vczf build_output/logstash_latest.tar.gz bin
+echo "------   	   TARBALLING  		-------"
+
+tar -vczf ../build_output/logstash_latest.tar.gz bin
+
 
