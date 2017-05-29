81c6b45b9acd5fba0a3f8dc556cd1043027d1c0d
stu-pete <nousefortoast@hotmail.de>
2017-05-29 15:48:50 +0200

    changes to eureka pipeline

--- task/eureka_build/run.sh
+++ task/eureka_build/run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 echo "Will build eureka now"
-mkdir build_output/eureka
 cd eureka_git
-gradle clean build
+./gradlew clean build
 
 cp eureka-server/build/libs/* ../eureka_build_output
