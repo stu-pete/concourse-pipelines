4cfba66f55f8de58427778894ad5baee6fb657d5
stu-pete <nousefortoast@hotmail.de>
2017-05-30 09:00:28 +0200

    added output to eureka task. changed directorys in both tasks

--- task/eureka_build/run.sh
+++ task/eureka_build/run.sh
@@ -5,5 +5,6 @@ cd eureka_git
 export TERM=${TERM:-dumb}
 ./gradlew clean build
 cp eureka-server/build/libs/* ../eureka_build_output
+tar -czf ../eureka_build_output/eureka_latest.tar.gz ../eureka_build_output/*
 
 echo "DONE"
